6504403b0aeb647bb0b7093a2882c3ab01e5a64b
raffichaesa <140777811+RaffiChaesa@users.noreply.github.com>
2024-01-02 18:23:16 +0700

    finishing navbar

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const fadeElems = document.querySelectorAll('.has-fade'); 
+
+btnHamburger.addEventListener('click', function () { 
+    // close hamburger menu
+    if (header.classList.contains('open')) {
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+    // open hamburger menu
+    else {
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+});
